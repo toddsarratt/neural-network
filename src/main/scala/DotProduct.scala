3ea159a353ec25175308fa753fbd28318b7edef9
toddsarratt <todd.sarratt@gmail.com>
2022-10-01 22:18:10 -0700

    NN-3 Refactor Neuron Dot Product function into more general object

---
+++ src/main/scala/DotProduct.scala
@@ -0,0 +1,17 @@
+import scala.annotation.tailrec
+
+object DotProduct {
+
+    def compute(xOuter: List[Double], yOuter: List[Double]): List[Double] = {
+        @tailrec
+        def dotProductWithAcc(xInner: List[Double], yInner: List[Double], acc: List[Double]): List[Double] = {
+            (xInner, yInner) match {
+                case (Nil, Nil) => acc
+                case (x :: xs, y :: ys) => dotProductWithAcc(xs, ys, acc :+ (x * y))
+            }
+        }
+
+        if (xOuter.length != yOuter.length) throw new IllegalArgumentException("Weights and inputs lengths differ")
+        else dotProductWithAcc(xOuter, yOuter, Nil)
+    }
+}
