b6c2df916c3af23abb1f9f15303d769f3ebbac9e
tsarratt <tsarratt@apexfintechsolutions.com>
2022-10-11 16:01:41 -0700

    NN-7 Slouching towards a solution

--- src/main/scala/numscala/SpiralData.scala
+++ src/main/scala/numscala/SpiralData.scala
@@ -3,7 +3,8 @@ package numscala
 import layers.Matrix
 import numscala.Numscala.linspace
 
-import scala.math.{cos, sin}
+import scala.annotation.tailrec
+import scala.math.{Pi, cos, sin}
 import scala.util.Random.nextGaussian
 
 /**
@@ -17,17 +18,33 @@ object SpiralData {
     val N = 100 // number of points per class
     val D = 2 // dimensionality
     val K = 3 // number of classes
-    val r: List[Double] = linspace(0.0,1,N) // radius
+    val r: List[Double] = linspace(0.0, 1, N) // radius
 
     def calculateDataMatrix(): Matrix = {
         for (j <- Range(0, K)) yield {
             val s = linspace(j * 4, (j + 1) * 4, N).zip(List.fill(N)(nextGaussian()).map(_ * 0.2))
             val t = for ((x, y) <- s)
                 yield x + y
-            val newPoints: Tuple2 = t.map(a => sin(a) * r).zip(t.map(a => cos(a) * r))
-            return Matrix.apply(
-                newPoints.map { point => List(point._1, point._2) }
-            )
+            // r is a LIST OF DOUBLES might be the issue
+            val newPoints = t.map(a => sin(a) * r).zip(t.map(a => cos(a) * r))
+            return Matrix.apply(newPoints)
+        }
+    }
+
+    def tuple2ToListDouble(tuples: List[(Double, Double)]): List[List[Double]] = {
+        @tailrec
+        def toList(tuples: List[(Double, Double)], acc: List[List[Double]]): List[List[Double]] = {
+            tuples match {
+                case Nil => acc
+                case _ =>
+                    val row = List(tuples.head._1, tuples.head._2)
+                    toList(tuples.tail, acc :+ row)
+            }
+        }
+
+        tuples match {
+            case Nil => Nil
+            case _ => toList(tuples, Nil)
         }
     }
 }
