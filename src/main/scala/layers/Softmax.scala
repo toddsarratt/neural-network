e7ab07b3b3f5d5524fbd14469d631710ee579926
tsarratt <tsarratt@apexfintechsolutions.com>
2022-11-26 13:41:36 -0800

    NN-10 Apply Softmax to spiral data

--- src/main/scala/layers/Softmax.scala
+++ src/main/scala/layers/Softmax.scala
@@ -2,13 +2,20 @@ package layers
 
 object Softmax {
 
-    def calculate(inputs: List[Double]): List[Double] = {
-        val normalizedInputs = inputs.map(normalize)
-        val sum = normalizedInputs.sum
-        normalizedInputs.map(_ / sum)
+    def forward(inputs: List[Double]): List[Double] = {
+        val maxValue = inputs.max
+        val expValues = inputs
+            .map(_ - maxValue) // Forces values into range -inf to 0 to prevent runaway exponentiation
+            .map(normalize)
+        val sum = expValues.sum
+        expValues.map(_ / sum)
     }
 
     def normalize(input: Double): Double = {
         scala.math.pow(Math.E, input)
+    }
+
+    def forward(input: Matrix): Matrix = {
+        Matrix.apply(input.value.map(forward))
     }
 }
