8333dba40dfb0c815b4aae2501e2c301222872c5
tsarratt <tsarratt@apexfintechsolutions.com>
2022-12-02 13:51:49 -0800

    NN-12 Comment update

--- src/main/scala/layers/Accuracy.scala
+++ src/main/scala/layers/Accuracy.scala
@@ -4,6 +4,9 @@ import numscala.Numscala
 
 object Accuracy {
 
+    // Takes a matrix and a list of class targets
+    // Iterates through the matrix and counts how many predictions (argmax) match the target for that "class"
+    // Divides the number of "hits" by the total number of classes to provide an "accuracy"
     def calculate(outputs: Matrix, classTargets: List[Int]): Double = {
         val predictions = for (row <- outputs.value)
             yield Numscala.argmax(row)
