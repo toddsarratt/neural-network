2a612703d9d095bf60737bc6d206c345caaf6e00
toddsarratt <todd.sarratt@gmail.com>
2022-12-03 09:20:08 -0800

    NN-12 Add support for class labels as one hot encoded matrix. Add accuracy to test that eventually should be pulled out to an integration test

--- src/main/scala/layers/Accuracy.scala
+++ src/main/scala/layers/Accuracy.scala
@@ -8,8 +8,20 @@ object Accuracy {
     // Iterates through the matrix and counts how many predictions (argmax) match the target for that "class"
     // Divides the number of "hits" by the total number of classes to provide an "accuracy"
     def calculate(outputs: Matrix, classTargets: List[Int]): Double = {
-        val predictions = for (row <- outputs.value)
-            yield Numscala.argmax(row)
+        val predictions = for (output <- outputs.value)
+            yield Numscala.argmax(output)
+        val matchingPredictions = predictions.zip(classTargets).collect {
+            case (prediction, target) if prediction == target => true
+        }
+        matchingPredictions.size.doubleValue / classTargets.size.doubleValue
+    }
+
+    // Handles a matrix of one-hot encoded targets, converting them to target labels
+    def calculate(outputs: Matrix, oneHotTargets: Matrix): Double = {
+        val predictions = for (output <- outputs.value)
+            yield Numscala.argmax(output)
+        val classTargets = for (target <- oneHotTargets.value)
+            yield Numscala.argmax(target)
         val matchingPredictions = predictions.zip(classTargets).collect {
             case (prediction, target) if prediction == target => true
         }
